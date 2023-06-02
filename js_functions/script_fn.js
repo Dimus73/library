27d3db2c7f35158d3d876f1846c4230778d68f01
Dimus73 <dmitry.prigozhin@gmail.com>
2023-06-02 12:15:50 +0300

    The model of the book has been changed. Search function example

---
+++ js_functions/script_fn.js
@@ -0,0 +1,55 @@
+// Functions
+console.log('Hello');
+
+async function getDataFromAPI(str_q){
+	try{
+		let charecter = await fetch(str_q);
+		let charecter_ob = await charecter.json();
+		return charecter_ob;
+	}catch{
+		let('Error');
+	}
+}
+
+
+// Get 40 books with image
+async function search40books(search){
+	let setOfBookObjects=[];
+	let lengthOfSet = 20;
+	let googlePosition = 0;
+	let googleStep = 40;
+	let q = search.split(' ').join('+');
+	console.log(q);
+	do {
+		let str_q = 'https://www.googleapis.com/books/v1/volumes?q=' + q;
+		str_q+= `&startIndex=${googlePosition}`;
+		str_q+= `&maxResults=${googleStep}`;
+
+		let response = await getDataFromAPI(str_q)
+		// console.log(response.items);
+
+		for (i of response.items){
+			if (setOfBookObjects.length < lengthOfSet){
+				try {
+					setOfBookObjects.push({
+					tytle:i.volumeInfo.title,
+					author:i.volumeInfo.authors.join(', '),
+					age_range:'???',
+					img:i.volumeInfo.imageLinks.thumbnail,
+					googl_id:i.volumeInfo.id
+				})
+				} catch (error) {
+					continue;
+				}
+			} else{
+				break
+			}
+		}
+		googlePosition += googleStep;
+	} while (setOfBookObjects.length < lengthOfSet)
+	console.log(setOfBookObjects);
+}
+
+//START PROGRAM
+let search = 'понедельник начинаетсяя в субботу'
+search40books(search);
--- library_top/main/models.py
+++ library_top/main/models.py
@@ -5,29 +5,30 @@ import datetime
 # Create your models here.
 
 class Books(models.Model):
-    title = models.CharField(max_length=50, blank=False)
-    author = models.ForeignKey('Author',on_delete=models.PROTECT) 
-    age_range = models.ForeignKey('Age_range',on_delete=models.PROTECT) 
-    img = models.ImageField(blank=True, null=True)
-    actual = models.BooleanField(default=True)
+    title        = models.CharField(max_length=50, blank=False)
+    author       = models.CharField(max_length=200, blank=True, null=True) 
+    age_range    = models.ForeignKey('Age_range',on_delete=models.PROTECT) 
+    img          = models.ImageField(blank=True, null=True)
+    googl_id     = models.CharField(max_length=20,blank=True,null=True)
+    actual       = models.BooleanField(default=True)
     def __str__ (self):
         return f"{self.title}"
 
     
 class Author(models.Model):
-    name = models.CharField(max_length=50,blank=False)
-    actual = models.BooleanField(default=True)
+    name        = models.CharField(max_length=50,blank=False)
+    actual      = models.BooleanField(default=True)
     def __str__ (self):
         return f"{self.name}"
 
 class Age_range(models.Model):
-    range = models.CharField(max_length=25, blank=False)
+    range      = models.CharField(max_length=25, blank=False)
     def __str__ (self):
         return f"{self.range}"
     
 class Library (models.Model):
-    book = models.ForeignKey(Books, on_delete=models.PROTECT)
-    user = models.ForeignKey(User,on_delete=models.PROTECT)
+    book      = models.ForeignKey(Books, on_delete=models.PROTECT)
+    user      = models.ForeignKey(User,on_delete=models.PROTECT)
     def __str__ (self):
         return f"{self.book.title} ovner {self.user.username}"
 
