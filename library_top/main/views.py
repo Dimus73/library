3be7e2d6c1bad0073d837190cb59f17b2ae36ae4
Dimus73 <dmitry.prigozhin@gmail.com>
2023-06-05 22:50:16 +0300

    Search books in Library

--- library_top/main/views.py
+++ library_top/main/views.py
@@ -1,16 +1,20 @@
 from django.forms import fields
+from django.http import JsonResponse, response
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import Age_range, Books, Library, UserProfile
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
 from rest_framework import generics, viewsets, permissions
-from .serializers import BooksSerializer, Age_rangeSerializer, BoobByGglIdSerializer, LibraryListSerializer, LibraryAddSerializer
+from .serializers import BooksSerializer, Age_rangeSerializer, BoobByGglIdSerializer, LibraryListSerializer, LibraryAddSerializer, LibrarySeachBookSerializer
 from rest_framework.views import APIView
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from django.db.models import Q
+import json
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -88,3 +92,43 @@ class UserProfileView(APIView):
             return Response ({'ok':True, 'user_profile_id':user_profile.id, 'message':'User profile created successfully'})
         except:
             return Response ({'ok':False, 'message':'Error creating user profile'})
+        
+
+class LibrarySeachBook (APIView):
+    def get(self, request):
+        title = request.GET.get('title', '')
+        author = request.GET.get('author', '')
+        age_range = request.GET.get('age_range', '')
+        print (title, author, age_range)
+
+        book_list = Library.objects.all()
+        if title != ' ':
+            print ('Ищем по заголовку', title)
+            book_list = book_list.filter(book__title__icontains=title)
+        if author != ' ':
+            print ('Ищем по заголовку', title)
+            book_list = book_list.filter(book__author__icontains=author)
+        search_data = []
+        for book in book_list:
+            search_data.append({
+                'id': book.pk,
+                'titel': book.book.title,
+                'author' : book.book.author,
+                'img' : book.book.img,
+                'name' : ' ' if book.user.first_name is None else book.user.first_name + \
+                        ' ' + ' ' if  book.user.last_name is None else book.user.last_name,
+                'addres' : ' ' if book.user.city is None else book.user.city + \
+                        ' ' + ' ' if  book.user.address is None else book.user.address,
+                # 'name' : 'Dmitry Prigozhin',
+                # 'addres' : 'Hyfa, hgalli25',
+                'phone' : book.user.phone,
+                'email' : book.user.email
+            })
+        print (search_data)
+        json_data = json.dumps(search_data)
+        return HttpResponse(json_data, content_type='application/json')
+    
+        # return JsonResponse(search_data)
+        # ser_data = LibrarySeachBookSerializer(search_data, many=True)
+        # print(ser_data.data)
+        # return (response(ser_data.validated_data))
